988a7dda252e849d8eb328d7267e4cd4ac9ba6f6
Georges Labrèche <botchan@artas-MacBook-Pro.local>
2017-03-15 01:09:33 +0100

    added more gif links

--- borderwait/settings.py
+++ borderwait/settings.py
@@ -37,20 +37,33 @@ WAIT_TIME_GIF_URLS = {
       'http://i.giphy.com/CAxbo8KC2A0y4.gif',
       'http://i.giphy.com/qCYJu1d0VfJTy.gif',
       'http://i.giphy.com/3otPoHqjMbo6JJ1FMk.gif',
-      'http://i.giphy.com/l2YWnw4TcCv5Vkgb6.gif'
+      'http://i.giphy.com/l2YWnw4TcCv5Vkgb6.gif',
+      'http://i.giphy.com/UjSdO0uoyGeac.gif',
+      'http://i.giphy.com/UiJ8YgFH2hlC.gif'
    ],
    'ok': [
       'http://i.giphy.com/3o7TKCGuEkIrLZ0E2Q.gif',
       'http://i.giphy.com/cPngiJPGKsfUQ.gif',
       'http://i.giphy.com/g790a3PzUHbs4.gif',
-      'http://i.giphy.com/RIymi3HoG8jNS.gif'
+      'http://i.giphy.com/RIymi3HoG8jNS.gif',
+      'http://i.giphy.com/QZbB48rGqylYk.gif',
+      'http://i.giphy.com/xT5LMFiDvePDSZtrtC.gif',
+      'http://i.giphy.com/xT0BKBY1YIfxxtk092.gif',
+      'http://i.giphy.com/11fucLQCTOdvBS.gif',
+      'http://i.giphy.com/pFkbkttdEBnSo.gif',
+      'http://i.giphy.com/g0MQ2Pyi5Z3s4.gif',
+      'http://i.giphy.com/9B5DEBbKQwwQE.gif'
    ],
    'bad': [
       'http://i.giphy.com/xTiTniwPRUeB59PNQc.gif',
       'http://i.giphy.com/bP2bOuWDpVM7C.gif',
       'http://i.giphy.com/NN5cAmTFRxpE4.gif',
       'http://i.giphy.com/H8VgqYcwGO47K.gif',
-      'http://i.giphy.com/rEKMO9OWtXjZS.gif'
+      'http://i.giphy.com/rEKMO9OWtXjZS.gif',
+      'http://i.giphy.com/3o6EhXIhX0HUX5nrsk.gif',
+      'http://i.giphy.com/d1EVJzlcmRz5VCx2.gif',
+      'http://i.giphy.com/br7gVfL2SRdJK.gif',
+      'http://i.giphy.com/VCEI9PfSssA8w.gif'
    ],
    'horrible': [
       'http://i.giphy.com/59KddieNEF2GQ.gif',
@@ -61,7 +74,10 @@ WAIT_TIME_GIF_URLS = {
       'http://i.giphy.com/iWlLpQbm0dZPq.gif',
       'http://i.giphy.com/4n9lYreyEcn3a.gif',
       'http://i.giphy.com/4jCxItUVMfHig.gif',
-      'http://i.giphy.com/nEL6rKDWEonGU.gif'
+      'http://i.giphy.com/nEL6rKDWEonGU.gif',
+      'http://i.giphy.com/xT5LMFVIKfCsH0rkoo.gif',
+      'http://i.giphy.com/l46CmHfsiZhKSERxe.gif',
+      'http://i.giphy.com/REPi7Zrx4xf5S.gif'
    ]
 }
 
