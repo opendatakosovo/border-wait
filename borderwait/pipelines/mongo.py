3f74ff7095ba5135cd9c1b88d8752325d2dba0a9
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-03-28 10:58:34 +0200

    checking if data is null bug fixed

--- borderwait/pipelines/mongo.py
+++ borderwait/pipelines/mongo.py
@@ -30,13 +30,18 @@ class MongoPipeline(object):
             border = item['border']
             time = item['time']
             latest_doc = self.db[self.mongo_collection].find({"border.border": border}).sort("timestamp", -1).limit(1)
-            for res in latest_doc:
-                if time == res['border']['time'] and border == res['border']['border']:
-                    self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
-                    raise DropItem("Duplicate item found: %s" % item)
-                else:
-                    self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
-                    return item
+            if latest_doc:
+                for res in latest_doc:
+                    if time == res['border']['time'] and border == res['border']['border']:
+                        self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
+                        raise DropItem("Duplicate item found: %s" % item)
+                    else:
+                        self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
+                        return item
+            else:
+                self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
+                return item
+
         else:
             self.db[self.mongo_collection].insert(dict({"timestamp" : datetime.datetime.now() , "border": item }))
             return item
