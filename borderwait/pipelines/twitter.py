55f4015d4929cbfa8bff2b6315ad9754c4c67300
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-04-03 14:02:28 +0200

    dowanloader now saves gifs to given direcctory in config

--- borderwait/pipelines/twitter.py
+++ borderwait/pipelines/twitter.py
@@ -2,12 +2,13 @@ import tweepy, os, requests, random
 
 # Tweet about the new item
 class TwitterPipeline(object):
-    def __init__(self, tw_consumer_key, tw_consumer_secret, tw_access_token, tw_access_token_secret, tw_gifs):
+    def __init__(self, tw_consumer_key, tw_consumer_secret, tw_access_token, tw_access_token_secret, tw_gifs, path_to_gifs):
         self.tw_consumer_key = tw_consumer_key
         self.tw_consumer_secret = tw_consumer_secret
         self.tw_access_token = tw_access_token
         self.tw_access_token_secret = tw_access_token_secret
         self.tw_gifs = tw_gifs
+        self.path_to_gifs = path_to_gifs
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
@@ -15,7 +16,8 @@ class TwitterPipeline(object):
             tw_consumer_secret=crawler.settings.get('TWITTER_CONSUMER_SECRET'),
             tw_access_token=crawler.settings.get('TWITTER_ACCESS_TOKEN'),
             tw_access_token_secret=crawler.settings.get('TWITTER_ACCESS_TOKEN_SECRET'),
-            tw_gifs=crawler.settings.get('WAIT_TIME_GIF_URLS')
+            tw_gifs=crawler.settings.get('WAIT_TIME_GIF_URLS'),
+            path_to_gifs=crawler.settings.get('GIFS_DIRECTORY')
         )
 
     def open_spider(self, spider):
@@ -47,33 +49,32 @@ class TwitterPipeline(object):
             else:
                 return feeling[3]
 
-        def get_random_feeling_url(feeling):
-            url = random.choice(self.tw_gifs[feeling])
-            return url
+        def get_random_feeling_path(feeling):
+            filelist = [ f for f in os.listdir('%s/%s/'%(self.path_to_gifs, feeling)) if f.endswith(".gif") ]
+            path = random.choice(filelist)
+            return '%s/%s/%s'%(self.path_to_gifs, feeling, path)
 
-        def tweet_gif(url, message):
-            gif = url.split('/')[3]
-            request = requests.get(url, stream=True)
-            self.tweepy_api.update_with_media(gif, status=message)
+        def tweet_gif(path, message):
+            self.tweepy_api.update_with_media(path, status=message)
 
         entry_feeling = get_feeling(entry_max)
         exit_feeling = get_feeling(exit_max)
 
         if entry_feeling is exit_feeling:
             # Generate 1 tweet message for both entry and exit.
-            gif_url = get_random_feeling_url(entry_feeling)
+            gif_path = get_random_feeling_path(entry_feeling)
             tw_message = 'Kufiri #%s: pritja 'u'\xeb''sht'u'\xeb'' %s deri %s minuta p'u'\xeb''r t'u'\xeb'' hyr'u'\xeb'' n'u'\xeb'' #Kosov'u'\xeb'' dhe %s deri %s minuta p'u'\xeb''r t'u'\xeb'' dalur.\n #Mir'u'\xeb''sevini #Rrug'u'\xeb''T'u'\xeb''Mbar'u'\xeb''' % (border,str(entry_min), str(entry_max), str(exit_min), str(exit_max))
             # tweet
-            tweet_gif(gif_url, tw_message)
+            tweet_gif(gif_path, tw_message)
         else:
             # Generate 2 tweet messages: one for entry and one for exit.
-            gif_url_entry = get_random_feeling_url(entry_feeling)
-            gif_url_exit = get_random_feeling_url(exit_feeling)
+            gif_path_entry = get_random_feeling_path(entry_feeling)
+            gif_path_exit = get_random_feeling_path(exit_feeling)
 
             tw_message_entry = 'Kufiri #%s: pritja 'u'\xeb''sht'u'\xeb'' %s deri %s minuta p'u'\xeb''r t'u'\xeb'' hyr'u'\xeb'' n'u'\xeb'' #Kosov'u'\xeb''. #Mir'u'\xeb''sevini' % (border,str(entry_min), str(entry_max))
             tw_message_exit = 'Kufiri #%s: pritja 'u'\xeb''sht'u'\xeb'' %s deri %s minuta p'u'\xeb''r t'u'\xeb''dal'u'\xeb'' nga #Kosova. #Rrug'u'\xeb''T'u'\xeb''Mbar'u'\xeb''' % (border, str(exit_min), str(exit_max))
 
             # tweet
-            tweet_gif(gif_url_entry, tw_message_entry)
-            tweet_gif(gif_url_exit, tw_message_exit)
+            tweet_gif(gif_path_entry, tw_message_entry)
+            tweet_gif(gif_path_exit, tw_message_exit)
         return item
--- downloader.py
+++ downloader.py
@@ -1,59 +1,21 @@
 import os, requests
+from borderwait import settings
 
-WAIT_TIME_GIF_URLS = {
-   'great': [
-      'http://i.giphy.com/kGMPV3ehtc7aE.gif',
-      'http://i.giphy.com/UKm1AF0UrCkb6.gif',
-      'http://i.giphy.com/qcMjXuCugvMXK.gif',
-      'http://i.giphy.com/A854pmlcoiHni.gif',
-      'http://i.giphy.com/cAfaWIcWr7qus.gif',
-      'http://i.giphy.com/mIMsLsQTJzAn6.gif',
-      'http://i.giphy.com/CAxbo8KC2A0y4.gif',
-      'http://i.giphy.com/qCYJu1d0VfJTy.gif',
-      'http://i.giphy.com/3otPoHqjMbo6JJ1FMk.gif',
-      'http://i.giphy.com/UjSdO0uoyGeac.gif',
-      'http://i.giphy.com/UiJ8YgFH2hlC.gif'
-   ],
-   'ok': [
-      'http://i.giphy.com/3o7TKCGuEkIrLZ0E2Q.gif',
-      'http://i.giphy.com/cPngiJPGKsfUQ.gif',
-      'http://i.giphy.com/g790a3PzUHbs4.gif',
-      'http://i.giphy.com/RIymi3HoG8jNS.gif',
-      'http://i.giphy.com/QZbB48rGqylYk.gif',
-      'http://i.giphy.com/xT0BKBY1YIfxxtk092.gif',
-      'http://i.giphy.com/11fucLQCTOdvBS.gif',
-      'http://i.giphy.com/pFkbkttdEBnSo.gif',
-      'http://i.giphy.com/g0MQ2Pyi5Z3s4.gif',
-      'http://i.giphy.com/9B5DEBbKQwwQE.gif'
-   ]
-   ,
-   'bad': [
-      'http://i.giphy.com/xTiTniwPRUeB59PNQc.gif',
-      'http://i.giphy.com/bP2bOuWDpVM7C.gif',
-      'http://i.giphy.com/NN5cAmTFRxpE4.gif',
-      'http://i.giphy.com/rEKMO9OWtXjZS.gif',
-      'http://i.giphy.com/3o6EhXIhX0HUX5nrsk.gif',
-      'http://i.giphy.com/br7gVfL2SRdJK.gif',
-   ],
-   'horrible': [
-      'http://i.giphy.com/59KddieNEF2GQ.gif',
-      'http://i.giphy.com/3o7TKFdhXnKtxbn6wg.gif',
-      'http://i.giphy.com/nAezPM5bCcuYw.gif',
-      'http://i.giphy.com/3o7qE3a5YpLpCdeq0U.gif',
-      'http://i.giphy.com/4n9lYreyEcn3a.gif',
-      'http://i.giphy.com/4jCxItUVMfHig.gif',
-      'http://i.giphy.com/nEL6rKDWEonGU.gif',
-      'http://i.giphy.com/xT5LMFVIKfCsH0rkoo.gif',
-      'http://i.giphy.com/l46CmHfsiZhKSERxe.gif',
-      'http://i.giphy.com/REPi7Zrx4xf5S.gif'
-   ]
-}
+directory = settings.GIFS_DIRECTORY
+WAIT_TIME_GIF_URLS = settings.WAIT_TIME_GIF_URLS
 
-for i in WAIT_TIME_GIF_URLS:
-    for j in WAIT_TIME_GIF_URLS[i]:
-        gif = j.split('/')[3]
-        request = requests.get(j, stream=True)
+feelings = ['great', 'ok', 'bad', 'horrible']
+for feels in feelings:
+    for url in WAIT_TIME_GIF_URLS[feels]:
+        gif = '%s/%s/%s'% (directory,feels,url.split('/')[3])
+        request = requests.get(url, stream=True)
         if request.status_code == 200:
+            if not os.path.exists(os.path.dirname(gif)):
+                try:
+                    os.makedirs(os.path.dirname(gif))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
             with open(gif, 'wb') as gf:
                 for chunk in request:
                     gf.write(chunk)
