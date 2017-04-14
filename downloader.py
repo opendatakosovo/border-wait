4e9ac8b9028e66c53cbefc28242a28047c6d2000
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-04-14 09:41:22 +0200

    downloader now check gifs size and deletes gifs

--- downloader.py
+++ downloader.py
@@ -1,14 +1,14 @@
 import os, requests
 from borderwait import settings
 
-directory = settings.GIFS_DIRECTORY
+gifs_directory = settings.GIFS_DIRECTORY
 WAIT_TIME_GIF_URLS = settings.WAIT_TIME_GIF_URLS
 
 feelings = ['great', 'ok', 'bad', 'horrible']
 for feels in feelings:
     for url in WAIT_TIME_GIF_URLS[feels]:
         gif_name = url.split('/')[3]
-        gif = '%s/%s/%s'% (directory,feels,gif_name)
+        gif = '%s/%s/%s'% (gifs_directory,feels,gif_name)
         if os.path.isfile(gif):
             print 'gif exitsts: %s' % gif_name
         else:
@@ -22,6 +22,12 @@ for feels in feelings:
                         if exc.errno != errno.EEXIST:
                             raise
                 with open(gif, 'wb') as gf:
-                    print 'saving gif %s' % gif_name
+                    print 'downloading gif %s' % gif_name
                     for chunk in request:
                         gf.write(chunk)
+                    # Tweepy allows posting gifs at max size of 3mb, so we check if gif is under 3mb.
+                    if os.path.getsize(gif) > 3000000:
+                        print 'size is too big, deleting file '+ gif_name
+                        os.remove(gif)
+                    else:
+                        print 'size is ok: %s' % os.path.getsize(gif)
