3c8cf440d37b0a30ba5b0cbd3570b73e60b86de9
arianithetemi <nitihetemi@gmail.com>
2018-02-23 02:31:23 +0100

    Uploading videos and updating status in fb video posts completed

--- borderwait/pipelines/facebook.py
+++ borderwait/pipelines/facebook.py
@@ -1,7 +1,7 @@
-import requests, random, sys, unicodedata
+import requests, random, sys, unicodedata, json
 from borderwait import message_generator
-# reload(sys)
-# sys.setdefaultencoding('utf-8')
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Post on Facebook about the new item
 class FacebookPipeline(object):
@@ -18,7 +18,8 @@ class FacebookPipeline(object):
         )
 
     def open_spider(self, spider):
-        self.fb_auth = 'https://graph.facebook.com/me/feed?access_token='+self.fb_access_token
+        self.fb_videos_url = 'https://graph-video.facebook.com/me/videos?access_token='+self.fb_access_token
+        self.fb_post_url = 'https://graph.facebook.com/?access_token='+self.fb_access_token
 
     def close_spider(self, spider):
         pass
@@ -52,32 +53,67 @@ class FacebookPipeline(object):
             else:
                 feeling[0]
 
-        def get_feeling_url(feeling, border):
-            normalized_border_name = unicodedata.normalize("NFD", border).encode("ascii", "ignore")
-            url = "http://207.154.242.169:81/images/" + feeling + "/" + normalized_border_name + ".gif"
-            return url.encode('utf-8').strip()
+        """
+            Getting the feeling value returned from get_feeling definition, and border name,
+            will find the right video based on feeling and border name,
+            returns the exact path of video in string
+        """
+        def get_video_feeling_path(feeling, border):
+            # Building the path based on border name and feeling
+            # filename = border_name + '_' + feeling + '.gif'
+            # gif_filename = filename.decode('utf-8')
+            # return './%s/%s'%(self.path_to_gifs, feeling, gif_filename)
+            return './videos/great/bernjak_great.mp4'
 
-        def fb_gif(url, message, feeling):
-            post_content = {'message': '%s' %(message), 'description': '%s' %(message),'link':str(url)}
-            facebook_post = requests.post(self.fb_auth, data=post_content).text
+        """ 
+            For posting a video in facebook timeline, we need to upload a video
+            at our fb account videos, then we need to make another request
+            for updating the status of that uploaded video
+        """
+        def post_video(video_path, message, feeling):
+            # Converting the video file into multipart/form-data format
+            multipart_video = open(video_path, 'rb')
 
+            # Preparing data content for upload video request
+            upload_data = {'file': multipart_video}
+
+            # Making API POST request for uploading this video
+            uploaded_video_response = requests.post(self.fb_videos_url, files=upload_data).text
+
+            # Loading as JSON from plain text the response from uploaded video
+            json_uploaded_video_response = json.loads(uploaded_video_response)
+
+            """
+                To update the status with the actual generated message
+                need to make another request to this latest uploaded video post
+            """
+
+            # Preparing the data content for updating status
+            data = {'id': json_uploaded_video_response['id'],
+                    'description': message}
+
+            # Making API POST request to update the status of this video uploaded post
+            updated_status_video_post_response = requests.post(self.fb_post_url, data=data).text
+
+
+        # Getting the feeling based on entry and exit maximum
         entry_feeling = get_feeling(entry_max)
         exit_feeling = get_feeling(exit_max)
 
         if entry_feeling is exit_feeling:
             # Generate 1 fb message for both entry and exit.
-            gif_url = get_feeling_url(entry_feeling, border)
+            gif_url = get_video_feeling_path(entry_feeling, border)
             fb_message = message_generator.enter_exit(border, entry_min, entry_max, exit_min, exit_max)
             # fb
-            fb_gif(gif_url, fb_message, entry_feeling)
+            post_video(gif_url, fb_message, entry_feeling)
         else:
             # Generate 2 fb messages: one for entry and one for exit.
-            gif_url_entry = get_feeling_url(entry_feeling, border)
-            gif_url_exit = get_feeling_url(exit_feeling, border)
+            gif_url_entry = get_video_feeling_path(entry_feeling, border)
+            gif_url_exit = get_video_feeling_path(exit_feeling, border)
 
             fb_message_entry = message_generator.enter(border, entry_min, entry_max)
             fb_message_exit = message_generator.exit(border, exit_min, exit_max)
 
             # fb
-            fb_gif(gif_url_entry, fb_message_entry, entry_feeling)
-            fb_gif(gif_url_exit, fb_message_exit, exit_feeling)
+            post_video(gif_url_entry, fb_message_entry, entry_feeling)
+            post_video(gif_url_exit, fb_message_exit, exit_feeling)
